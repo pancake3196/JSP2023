0096606560ed4c1a4be4639dfbbc7e99ed895b5e
kenu <kenu@okjsp.pe.kr>
2023-11-06 11:45:20 +0900

    connector

--- door/src/main/java/door/ConnectTest.java
+++ door/src/main/java/door/ConnectTest.java
@@ -1,16 +1,15 @@
 package door;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class ConnectTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException {
 		try {
-			Connection connection = getConnection();
+			Connection connection = new Connector().getConnection();
 			
 			// SQL 보내기
 			String sql = "select * from users";
@@ -26,19 +25,9 @@ public class ConnectTest {
 			ps.close();
 			connection.close();
 			
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-	}
-
-	public static Connection getConnection() throws ClassNotFoundException, SQLException {
-		// 드라이버 로딩
-		Class.forName("org.mariadb.jdbc.Driver");
-		// 커넥션 맺기
-		Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:3307/devdb", "devuser", "devpass");
-		return connection;
 	}
 
 }
